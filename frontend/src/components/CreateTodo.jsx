cb22c08ed60db68257e98871880d972663fd0196
adityasrivastav5 <adityasrivastav863@gmail.com>
2024-05-28 17:17:29 +0530

    added component

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,14 @@
+
+
+const CreateTodo = () => {
+  return (
+    <div>
+        <input type='text' placeholder='Title'></input> <br></br> <br />
+        <input type='text' placeholder='Description'></input> <br></br> <br />
+
+        <button>Add Todo</button>
+    </div>
+  )
+}
+
+export default CreateTodo
