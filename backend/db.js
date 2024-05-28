19aaeb6a4ed8fe3723b65e105121c33627ecc982
adityasrivastav5 <adityasrivastav863@gmail.com>
2024-05-28 15:28:12 +0530

    new commit

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { string } = require("zod");
+//mongodb+srv://adityasrivastav863:iamaditya05@cluster0.icvsofp.mongodb.net/todos
+mongoose.connect("mongodb+srv://adityasrivastav863:iamaditya05@cluster0.icvsofp.mongodb.net/todos")
+const todoSchema = mongoose.Schema({
+    title : String,
+    description : String,
+    completed : Boolean
+})
+
+const todo = mongoose.model("todos", todoSchema);
+
+module.exports = {
+    todo : todo
+}
